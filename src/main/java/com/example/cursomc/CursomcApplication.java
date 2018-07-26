f85c2d0ecace1baa3ab73c44a110b82ef3946446
Andson Josef <andson.josef@gmail.com>
2018-07-26 01:58:04 -0300

    creating instance operation

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.example.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.cursomc.domain.Category;
+import com.example.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "computing");
+		Category cat2 = new Category(null, "office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+		
 	}
 }
