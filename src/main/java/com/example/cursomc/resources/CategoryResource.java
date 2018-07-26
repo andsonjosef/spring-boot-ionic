051ba085d99eb3a1a1d55324a53328c494cc822b
Andson Josef <andson.josef@gmail.com>
2018-07-25 23:01:23 -0300

    Testing first domain class - Category

--- src/main/java/com/example/cursomc/resources/CategoryResource.java
+++ src/main/java/com/example/cursomc/resources/CategoryResource.java
@@ -1,17 +1,30 @@
 package com.example.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cursomc.domain.Category;
+
 @RestController
-@RequestMapping(value="/categories")
+@RequestMapping(value = "/categories")
 public class CategoryResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Category> list() {
+
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "office");
+
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
 		
-		return "Rest is working";
+		return list;
+
 	}
 
 }
