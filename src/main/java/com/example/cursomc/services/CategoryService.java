587577617bc1fa6df608471774e8251d188b7b6f
Andson Josef <andson.josef@gmail.com>
2018-07-28 02:35:58 -0300

    updating Category using PUT

--- src/main/java/com/example/cursomc/services/CategoryService.java
+++ src/main/java/com/example/cursomc/services/CategoryService.java
@@ -14,15 +14,20 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repo;
-	
+
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Obect not found! Id: " + id + ", Type: " + Category.class.getName()));
 	}
-	
+
 	public Category insert(Category obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Category update(Category obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
