e708a33957639b30956aebed5c3a02d3cce9f008
Andson Josef <andson.josef@gmail.com>
2018-07-28 01:07:13 -0300

    update: using only jsonIgnore

--- src/main/java/com/example/cursomc/domain/Client.java
+++ src/main/java/com/example/cursomc/domain/Client.java
@@ -15,8 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.example.cursomc.domain.enums.TypeClient;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Client implements Serializable {
@@ -30,7 +29,6 @@ public class Client implements Serializable {
 	private String TID;
 	private Integer type;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="client")
 	private List<Address> adresses = new ArrayList<>();
 
@@ -38,7 +36,7 @@ public class Client implements Serializable {
 	@CollectionTable(name="phone")
 	private Set<String> phones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="client")
 	private List<Order> orders = new ArrayList<>();
 
