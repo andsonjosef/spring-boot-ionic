edbc417562d1e602b9f5fb581761bf46cac0f7b4
Andson Josef <andson.josef@gmail.com>
2018-08-02 07:38:35 -0300

    custom validation: not repeated email when inserting a new Client

--- src/main/java/com/example/cursomc/services/validation/ClientInsertValidator.java
+++ src/main/java/com/example/cursomc/services/validation/ClientInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.cursomc.domain.Client;
 import com.example.cursomc.domain.enums.TypeClient;
 import com.example.cursomc.dto.ClientNewDTO;
+import com.example.cursomc.repositories.ClientRepository;
 import com.example.cursomc.resources.exception.FieldMessage;
 import com.example.cursomc.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, ClientNewDTO> {
+
+	@Autowired
+	private ClientRepository repo;
+
 	@Override
 	public void initialize(ClientInsert ann) {
 	}
@@ -19,14 +27,20 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 	@Override
 	public boolean isValid(ClientNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
-		if(objDto.getType().equals(TypeClient.INDIVIDUAL.getCod()) && !BR.isValidCPF(objDto.getTid())) {
+
+		if (objDto.getType().equals(TypeClient.INDIVIDUAL.getCod()) && !BR.isValidCPF(objDto.getTid())) {
 			list.add(new FieldMessage("tid", "Invalid individual tid(CPF)"));
 		}
-		
-		if(objDto.getType().equals(TypeClient.CORPORATION.getCod()) && !BR.isValidCNPJ(objDto.getTid())) {
+
+		if (objDto.getType().equals(TypeClient.CORPORATION.getCod()) && !BR.isValidCNPJ(objDto.getTid())) {
 			list.add(new FieldMessage("tid", "Invalid corporation tid(CNPJ)"));
 		}
+
+		Client aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email", "existing email"));
+		}
+		
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
