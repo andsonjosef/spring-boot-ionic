4f620021f6a7d2a1b9cb3f68f9d4d6b729170ad2
Andson Josef <andson.josef@gmail.com>
2018-08-08 09:18:04 -0300

    implementing SmtpEmailService using google server

--- src/main/java/com/example/cursomc/config/TestConfig.java
+++ src/main/java/com/example/cursomc/config/TestConfig.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.example.cursomc.services.DBService;
-import com.example.cursomc.services.EmailService;
-import com.example.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -24,11 +22,5 @@ public class TestConfig {
 		dbService.IstantiateTestDataBase();
 		return true;
 	}
-	
-	@Bean
-	public EmailService emailService() {
-		return new MockEmailService();
-	}
-
 
 }
